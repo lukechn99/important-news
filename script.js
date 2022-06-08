83576e1a878b09a7b52685a5673cd5294e87cbf8
lukechn99 <47294899+lukechn99@users.noreply.github.com>
2022-06-08 12:34:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,21 @@
 async function fetchData() {
     const res=await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
     const record=await res.json();
-    let systemData = {}
+    var archName, modelName, numOfProcessors;
     // document.getElementById("date").innerHTML=record.data[0].date;
     // document.getElementById("areaName").innerHTML=record.data[0].areaName;
     // document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
     // document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
-    chrome.system.cpu.getInfo(function(info){       
+    chrome.system.cpu.getInfo(function(info){
+        console.log(info);
+        archName = info.archName;
+        console.log(archName);
+
         systemData=info;  
-     });
-    document.getElementById("cpu").innerHTML=systemData;
+        document.getElementById("cpu").innerHTML=archName;
+
+    });
+    
 
 }
 fetchData();
